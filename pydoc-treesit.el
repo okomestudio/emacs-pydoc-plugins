16caff3f088da6d3e882dbfee5dfaf3ada9bc4b4
Taro Sato <okomestudio@gmail.com>
2025-07-30 01:11:02 -0700

    Refactor code

--- pydoc-treesit.el
+++ pydoc-treesit.el
@@ -33,21 +33,18 @@
 (require 'pydoc)
 
 (defun pydoc-treesit--path-to-identifier (node)
-  "Get the partial path to identifier at treesit NODE."
-  (when-let* ((node (and (equal (treesit-node-type node)
-                                "identifier")
-                         (treesit-node-at (point)))))
-    (let* ((parent (treesit-node-parent node))
-           (object (treesit-node-child-by-field-name parent "object"))
-           (attribute (treesit-node-child-by-field-name parent "attribute"))
-           (identifier (treesit-node-text node)))
-      (if (and parent (equal (treesit-node-text attribute) identifier))
-          (append (split-string (treesit-node-text object) "\\.")
-                  `(,identifier))
-        `(,identifier)))))
+  "Get the (dotted) path components as a list to identifier at treesit NODE."
+  (let* ((parent (treesit-node-parent node))
+         (object (treesit-node-child-by-field-name parent "object"))
+         (attribute (treesit-node-child-by-field-name parent "attribute"))
+         (identifier (treesit-node-text node)))
+    (if (and parent (equal (treesit-node-text attribute) identifier))
+        (append (split-string (treesit-node-text object) "\\.")
+                `(,identifier))
+      `(,identifier))))
 
 (defun pydoc-treesit--package-root (&optional path)
-  "Search upward from PATH for the Python package root directory.
+  "Search upward along filesystem PATH for the Python package root directory.
 When not given, PATH defaults to the value of function `buffer-file-name'."
   (let* ((dir (or path (file-name-directory (buffer-file-name))))
          (init-file (expand-file-name "__init__.py" dir)))
@@ -63,24 +60,30 @@ When not given, PATH defaults to the value of function `buffer-file-name'."
         (when (not (equal parent dir))
           (pydoc-treesit--package-root parent)))))))
 
+(defun pydoc-treesit--this-module ()
+  "Get the name of the Python module for the current buffer."
+  (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
+
+(defun pydoc-treesit--this-directory ()
+  "Get the directory path containing the file visited by the current buffer."
+  (directory-file-name (file-name-directory (buffer-file-name))))
+
 (defun pydoc-treesit--fully-qualify-path (path)
   "Fully resolve Python module PATH when given relatively."
-  (if-let*
-      ((num-dots (if (string-match "\\`\\.+\\b" path)
-                     (length (match-string 0 path))))
-       (package-root (pydoc-treesit--package-root))
-       (module-path-comps
-        (seq-remove
-         (lambda (it) (string= it ""))
-         `(,(file-name-nondirectory (directory-file-name package-root))
-           ,@(string-split
-              (string-remove-prefix
-               package-root
-               (directory-file-name (file-name-directory (buffer-file-name))))
-              (regexp-opt (list "/" "\\"))))))
-       (module-path (string-join (butlast module-path-comps (1- num-dots))
-                                 ".")))
-      (if (eq num-dots (length path))
+  (if-let* ((num-dots (if (string-match "\\`\\.+\\b" path)
+                          (length (match-string 0 path))))
+            (package-root (pydoc-treesit--package-root))
+            (module-path-comps
+             (seq-remove
+              (lambda (it) (string= it ""))
+              `(,(file-name-nondirectory (directory-file-name package-root))
+                ,@(string-split
+                   (string-remove-prefix package-root
+                                         (pydoc-treesit--this-directory))
+                   (regexp-opt (list "/" "\\"))))))
+            (module-path (string-join (butlast module-path-comps (1- num-dots))
+                                      ".")))
+      (if (length= path num-dots)
           module-path
         (concat module-path "." (substring path num-dots)))
     path))
@@ -93,15 +96,15 @@ When not given, PATH defaults to the value of function `buffer-file-name'."
              alias: (identifier) @alias-1)])
     (import_from_statement
      module_name: [(relative_import (_) @module-relative-2)
-                   (dotted_name (identifier) @module-2)]
+                   (dotted_name (_) @module-2)]
      name: [(dotted_name (identifier) @name-2)
             (aliased_import
              name: (dotted_name (identifier) @name-2-with-alias)
              alias: (identifier) @alias-2)]))
   "Queries for Python import statements.")
 
-(defun pydoc-treesit--partial-match-import-path (identifier)
-  "Given an IDENTIFIER, return a matching import path.
+(defun pydoc-treesit--partial-match-import-path (identifier &optional node)
+  "Given an IDENTIFIER in NODE, return a matching import path.
 IDENTIFIER may be a qualified name (i.e., dotted name). In that case, prefix
 matching is performed against imported names."
   (let* ((capture (cl-find-if
@@ -114,47 +117,78 @@ matching is performed against imported names."
     (pcase (car capture)
       ('name-1
        (let* ((p (treesit-node-parent c)))
-         `(,(treesit-node-text p))))
+         (list (treesit-node-text p))))
       ('alias-1
        (let* ((p (treesit-node-parent c))
               (a (treesit-node-child-by-field-name p "name")))
-         `(,(treesit-node-text a))))
+         (list (treesit-node-text a))))
       ('name-2
        (let* ((p (treesit-node-parent c))
               (q (treesit-node-parent p))
               (m (treesit-node-child-by-field-name q "module_name")))
-         `(,(pydoc-treesit--fully-qualify-path (treesit-node-text m))
-           ,(treesit-node-text c))))
+         (list (pydoc-treesit--fully-qualify-path (treesit-node-text m))
+               (treesit-node-text c))))
       ('alias-2
        (let* ((p (treesit-node-parent c))
               (a (treesit-node-child-by-field-name p "name"))
               (q (treesit-node-parent p))
               (m (treesit-node-child-by-field-name q "module_name")))
-         `(,(pydoc-treesit--fully-qualify-path (treesit-node-text m))
-           ,(treesit-node-text a)))))))
+         (list (pydoc-treesit--fully-qualify-path (treesit-node-text m))
+               (treesit-node-text a))))
+      ('module-2
+       (when-let*
+           ((on-from-import-stmt
+             (equal "import_from_statement"
+                    (treesit-node-type (treesit-node-parent
+                                        (treesit-node-parent node)))))
+            (p (treesit-node-parent c))
+            (l (string-split (treesit-node-text p) "\\.")))
+         (take (1+ (cl-position (treesit-node-text c) l :test #'equal)) l)))
+      ('module-relative-2
+       (when-let*
+           ((on-relative-import
+             (equal "relative_import"
+                    (treesit-node-type (treesit-node-parent
+                                        (treesit-node-parent node)))))
+            (p (treesit-node-parent c))
+            (l (string-split (pydoc-treesit--fully-qualify-path (treesit-node-text p))
+                             "\\.")))
+         (take (1+ (cl-position (treesit-node-text c) l :test #'equal)) l)))
+      )))
 
 (defun pydoc-treesit--resolve-to-full-path (node)
   "Resolve treesit NODE to the full path."
-  (if-let* ((parts (pydoc-treesit--path-to-identifier node)))
-      (let* ((tot (length parts))
-             (len tot)
-             matched)
-        (while (not (or matched (eq len 0)))
-          (setq matched (pydoc-treesit--partial-match-import-path
-                         (string-join (take len parts) ".")))
-          (when matched
-            (setq matched (append matched (last parts (- tot len)))))
-          (setq len (1- len)))
-        (string-join
-         (if matched
-             matched
-           (append (list (pydoc-treesit--fully-qualify-path ".")
-                         (file-name-sans-extension
-                          (file-name-nondirectory (buffer-file-name))))
-                   parts))
-         "."))
-    ;; The non-identifier node here is likely a keyword.
-    (treesit-node-text node)))
+  (cond
+   ((equal (treesit-node-type node) "identifier")
+    (let* ((parts (pydoc-treesit--path-to-identifier node))
+           (num-parts (length parts))
+           (len num-parts)
+           matched)
+      (while (not (or matched (eq len 0)))
+        (setq matched (pydoc-treesit--partial-match-import-path
+                       (string-join (take len parts) ".")
+                       node))
+        (when matched
+          (setq matched (append matched (last parts (- num-parts len)))))
+        (setq len (1- len)))
+      (string-join (cond
+                    ((not (null matched))
+                     matched)
+                    ((and (eq (length parts) 1)
+                          (member (car parts) (pydoc-builtin-objects)))
+                     parts)
+                    (t
+                     (append (list (pydoc-treesit--fully-qualify-path ".")
+                                   (pydoc-treesit--this-module))
+                             parts)))
+                   ".")))
+
+   ((and (equal (treesit-node-type node) ".")
+         (equal "import_prefix" (treesit-node-type (treesit-node-parent node))))
+    (pydoc-treesit--fully-qualify-path (treesit-node-text (treesit-node-parent node))))
+
+   ;; the non-identifier node is likely a keyword here
+   (t (treesit-node-text node))))
 
 ;;;###autoload
 (defun pydoc-treesit-at-point ()
@@ -162,7 +196,7 @@ matching is performed against imported names."
   (interactive)
   (if (derived-mode-p '(python-ts-mode))
       (let ((s (pydoc-treesit--resolve-to-full-path (treesit-node-at (point)))))
-        (pydoc s))
+        (pydoc (shell-quote-argument s)))
     (message "Not in `python-ts-mode'")))
 
 (provide 'pydoc-treesit)
